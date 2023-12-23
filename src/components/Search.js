e6d351401397152b82b1d4394a67dca556899257
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2023-12-23 19:09:42 +0530

    SOME MINOR CHANGE

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,24 +3,34 @@ import axios from 'axios';
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState(null);
   const [pokemonImageUrl, setPokemonImageUrl] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const handleSearch = async (event) => {
-    event.preventDefault(); 
-    setSearchTerm(event.target.value); 
+  const fetchData = async (newSearchTerm) => {
+    setIsLoading(true);
+    setError(null);
 
     try {
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
-
-      
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${newSearchTerm.toLowerCase()}`);
       const pokemonId = response.data.id;
       setPokemonImageUrl(`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemonId}.svg`);
-
       setSearchResults(response.data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      setError(error);
+    } finally {
+      setIsLoading(false);
     }
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    fetchData(searchTerm);
+  };
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
   };
 
   return (
@@ -29,17 +39,19 @@ const Search = () => {
         <input
           type="text"
           value={searchTerm}
-          onChange={handleSearch}
+          onChange={handleInputChange}
           className="w-full px-4 py-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Search Pokemon by name or ID"
         />
+        {isLoading && <p className="text-center mt-2">Loading...</p>}
       </form>
-      {searchResults.length > 0 && (
+      {searchResults && (
         <div className="mt-4">
           <img src={pokemonImageUrl} alt={searchResults.name} className="w-20 h-20 rounded-full" />
           <h4 className="text-lg font-bold text-gray-900">{searchResults.name}</h4>
         </div>
       )}
+      {error && <p className="text-red-500">{error.message}</p>}
     </div>
   );
 };
